2ab52ae95f4976358cc01063b9a38d9165b1974c
Joseph Earl <joseph@lenioapp.com>
2016-04-28 10:29:42 +0100

    Update copyright notice

--- src/main/scala/uk/co/josephearl/sbt/verify/VerifyOptions.scala
+++ src/main/scala/uk/co/josephearl/sbt/verify/VerifyOptions.scala
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2015-2016 Joseph Earl & contributors.
+ * All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the terms of the under the Apache License,
+ * Version 2.0 which accompanies this distribution, and is available at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
 package uk.co.josephearl.sbt.verify
 
 import sbt.Keys._
